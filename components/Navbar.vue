92efebf9f01d6487323a31782c8ee5bde9e18073
Gregory <gregoadr@gmai.com>
2022-10-11 20:58:44 -0500

    Layout Principal

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <vs-navbar v-model="active" center-collapsed>
+      <template #right>
+        <a-dropdown>
+          <a-menu slot="overlay">
+            <a-menu-item key="1">
+              <a-icon type="info-circle" /> Información
+            </a-menu-item>
+            <a-menu-item key="2">
+              <a-icon type="logout" /> Cerrar Sesión
+            </a-menu-item>
+          </a-menu>
+          <a-button shape="circle">
+            <a-icon type="user" />
+          </a-button>
+        </a-dropdown>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    active: 'home',
+    activeSidebar: false
+  })
+})
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+    <Slidebar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '@/components/Navbar.vue'
+import Slidebar from '@/components/Slidebar.vue'
+
+export default Vue.extend({
+  name: 'DefaultLayout',
+  components: { Navbar, Slidebar }
+
+})
+</script>
